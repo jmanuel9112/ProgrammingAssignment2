593a092a8e50e1a6f9d9ff1efdb7ceedb551480c
jmanuel9112 <jmanuel9112@icloud.com>
2014-05-20 19:56:27 -0500

    Added code to the makeCacheMatrix function in cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,8 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    set <- function(y, row = 2, col = 2) {                  #creates a 2x2 matrix by default
+        x <<-matrix(y, nrow = row, ncol = col)
+    }
+    get <- function() x
+    inverse  <- function() solve(x)
+    list(set = set, get = get,
+         inverse = inverse)
 }
+
 
 
 ## Write a short comment describing this function
